310ac8f2953e0ed55ea45ab00fb1636be39d61df
Kyle Lawson CESM2016 <kyle.lawson.2016@uni.strath.ac.uk>
2019-05-11 12:13:15 +0100

    Replace ESP_app.ino

--- Kyle- Microcontroller + ESP code/ESP/ESP_app.ino
+++ Kyle- Microcontroller + ESP code/ESP/ESP_app.ino
@@ -1,88 +1,152 @@
-#include <BearSSLHelpers.h>
-#include <CertStoreBearSSL.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WiFiAP.h>
-#include <ESP8266WiFiGeneric.h>
 #include <ESP8266WiFiMulti.h>
-#include <ESP8266WiFiScan.h>
-#include <ESP8266WiFiSTA.h>
-#include <ESP8266WiFiType.h>
-#include <WiFiClient.h>
-#include <WiFiClientSecure.h>
-#include <WiFiClientSecureAxTLS.h>
-#include <WiFiClientSecureBearSSL.h>
-#include <WiFiServer.h>
-#include <WiFiServerSecure.h>
-#include <WiFiServerSecureAxTLS.h>
-#include <WiFiServerSecureBearSSL.h>
-#include <WiFiUdp.h>
-#include <string.h>
+#include <ArduinoOTA.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <FS.h>
+#include <WebSocketsServer.h>
 
-// 192.168.43.241 is the IP to use!
+ESP8266WiFiMulti wifiMulti;       // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
 
-const char* ssid = "Kyle's OnePlus 6t"; //your WiFi Name
-const char* password = "star wars";  //Your Wifi Password
+ESP8266WebServer server(80);       // create a web server on port 80
+WebSocketsServer webSocket(81);    // create a websocket server on port 81
+
+const char *ssid = "ESP8266 Access Point"; // The name of the Wi-Fi network that will be created
+const char *password = "thereisnospoon";   // The password required to connect to it, leave blank for an open network
+
+const char *OTAName = "ESP8266";           // A name and a password for the OTA service
+const char *OTAPassword = "esp8266";
+
+const char* mdnsName = "esp8266"; // Domain name for the mDNS responder
+
 int fanPin = 16; // D0
 int heaterPin = 05; // D1
 int PIRin = 04; // D2
-//int con = 00; // D3
-//int PIRin = 02; // D4
-WiFiServer server(1337);
+/*__________________________________________________________SETUP__________________________________________________________*/
+
 void setup() {
-  WiFi.begin(ssid, password);
   pinMode(fanPin, OUTPUT);
   pinMode(heaterPin, OUTPUT);
-  //pinMode(con, OUTPUT);
-  //pinMode(noCon, OUTPUT);
   pinMode(PIRin, INPUT);
-  //digitalWrite(con, LOW);
-  //digitalWrite(noCon, LOW);
+
   digitalWrite(fanPin, LOW);
   digitalWrite(heaterPin, LOW);
-  // Start TCP server.
-  server.begin();
+
+  Serial.begin(115200);        // Start the Serial communication to send messages to the computer
+  delay(10);
+  Serial.println("\r\n");
+
+  startWiFi();                 // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+
+  startOTA();                  // Start the OTA service
+
+  startWebSocket();            // Start a WebSocket server
+
+  startMDNS();                 // Start the mDNS responder
+
+  // startServer();               // Start a HTTP server with a file read handler and an upload handler
+
 }
 
+/*__________________________________________________________LOOP__________________________________________________________*/
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (WiFi.status() != WL_CONNECTED) {
-    while (WiFi.status() != WL_CONNECTED) {
-    //  digitalWrite(noCon, HIGH);
-      delay(500);
-    }
-   // digitalWrite(noCon, LOW);
-   // digitalWrite(con, HIGH);
+  webSocket.loop();                           // constantly check for websocket events
+  server.handleClient();                      // run the server
+  ArduinoOTA.handle();                        // listen for OTA events
+  if (digitalRead(PIRin)) {
+    String str = "alert";
+    webSocket.sendTXT(0, str);
   }
+}
 
-  WiFiClient client = server.available();
+/*__________________________________________________________SETUP_FUNCTIONS__________________________________________________________*/
 
-  if (client) {
-    while (client.connected()) {
-      if (client.available()) {
-        char readChar = client.read();
-        String command = "";
+void startWiFi() { // Start a Wi-Fi access point, and try to connect to some given access points. Then wait for either an AP or STA connection
+  WiFi.softAP(ssid, password);             // Start the access point
+  Serial.print("Access Point \"");
+  Serial.print(ssid);
+  Serial.println("\" started\r\n");
 
-        while (readChar != '\n') {
-          command += readChar;
-          readChar = client.read();
-        }
-        if (command.equals("F_H")) {
-          digitalWrite(fanPin, !digitalRead(fanPin));
-        }
-        else if (command == "H_H") {
-          digitalWrite(heaterPin, !digitalRead(heaterPin));
-        }
-        if (digitalRead(PIRin)) {
-          client.println("GET /alert HTTP/1.0");
-          client.println();
-//          client.print(String("GET /alert") + " HTTP/1.1\r\n" +
-//                       "Host: NODEMCU\r\n" +
-//                       "Connection: close\r\n" +
-//                       "\r\n"
-//                      );
-        }
+  wifiMulti.addAP("Kyle's OnePlus 6t", "star wars");   // add Wi-Fi networks you want to connect to
+
+  Serial.println("Connecting");
+  while (wifiMulti.run() != WL_CONNECTED && WiFi.softAPgetStationNum() < 1) {  // Wait for the Wi-Fi to connect
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println("\r\n");
+  if (WiFi.softAPgetStationNum() == 0) {     // If the ESP is connected to an AP
+    Serial.print("Connected to ");
+    Serial.println(WiFi.SSID());             // Tell us what network we're connected to
+    Serial.print("IP address:\t");
+    Serial.print(WiFi.localIP());            // Send the IP address of the ESP8266 to the computer
+  } else {                                   // If a station is connected to the ESP SoftAP
+    Serial.print("Station connected to ESP8266 AP");
+  }
+  Serial.println("\r\n");
+}
+
+void startOTA() { // Start the OTA service
+  ArduinoOTA.setHostname(OTAName);
+  ArduinoOTA.setPassword(OTAPassword);
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Start");
+    // TODO initialise pins
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\r\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("OTA ready\r\n");
+}
+
+void startWebSocket() { // Start a WebSocket server
+  webSocket.begin();                          // start the websocket server
+  webSocket.onEvent(webSocketEvent);          // if there's an incomming websocket message, go to function 'webSocketEvent'
+  Serial.println("WebSocket server started.");
+}
+
+void startMDNS() { // Start the mDNS responder
+  MDNS.begin(mdnsName);                        // start the multicast domain name server
+  Serial.print("mDNS responder started: http://");
+  Serial.print(mdnsName);
+  Serial.println(".local");
+}
+
+void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) { // When a WebSocket message is received
+  switch (type) {
+    case WStype_DISCONNECTED:             // if the websocket is disconnected
+      Serial.printf("[%u] Disconnected!\n", num);
+      break;
+    case WStype_CONNECTED: {              // if a new websocket connection is established
+        IPAddress ip = webSocket.remoteIP(num);
+        Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
       }
-    }
-    client.stop();
+      break;
+    case WStype_TEXT:
+      Serial.printf("[%u] Text: %s\n", num, payload);
+      if (strcmp((const char *)payload, "F_H\n") == 0) {
+        digitalWrite(fanPin, !digitalRead(fanPin));
+      }
+      else if (strcmp((const char *)payload, "H_H\n") == 0) {
+        digitalWrite(heaterPin, !digitalRead(heaterPin));
+      }else {
+        Serial.printf("Did not match");
+        Serial.printf((char*)payload);
+      }
+      break;
   }
 }
