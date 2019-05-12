0361695d1b98cfd9b8b65dc75e6d1bff65218411
Kyle Lawson CESM2016 <kyle.lawson.2016@uni.strath.ac.uk>
2019-05-13 00:31:12 +0100

    Just the raw, *base* complete control page

--- Kyle- Microcontroller + ESP code/MSP/sensorDriver.h
+++
@@ -1,7 +0,0 @@
-#include <msp430.h>
-#include <driverlib.h>
-
- extern void initialiseSensors();
- extern void toggleFans(int state);
- extern void toggleHeater(int state);
- extern void signalMotion(int state);
